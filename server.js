c52f899d07d238a0baa4035749c430b83e9d3255
Victor Awotidebe <victorawotidebe@gmail.com>
2019-02-09 12:23:06 +0100

    Add API Endpoints

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Reflection from "./controllers/Reflection";
 
 const app = express();
 
@@ -7,6 +8,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
     return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
 });
+
+app.post('/api/v1/reflections', Reflection.create);
+app.get('/api/v1/reflections', Reflection.getAll);
+app.get('/api/v1/reflections/:id', Reflection.getOne);
+app.put('/api/v1/reflections/:id', Reflection.update);
+app.delete('/api/v1/reflections', Reflection.delete);
 
 const port = process.env.PORT || 3000;
 
