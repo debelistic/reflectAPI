3c0101d1341731250a85d950d2085f8d78b28475
Victor Awotidebe <victorawotidebe@gmail.com>
2019-02-09 08:51:57 +0100

    Setup data Modle

--- models/Reflection.js
+++ models/Reflection.js
@@ -9,7 +9,7 @@ class Reflection {
      */
 
     constructor(){
-        this.Reflection = [];
+        this.reflections = [];
     }
     /**
      * @return {object} reflection object
@@ -24,13 +24,53 @@ class Reflection {
              createdDate: moment.now(),
              modifiedDate: moment.now()
          };
-         this.Reflection.push(newReflection);
+         this.reflections.push(newReflection);
          return newReflection
      }
      
      /**
       * 
       * @param {uuid} id
-      * 
+      * @returns {object} refelction object
       */
+     findOne(id){
+         return this.reflections.find(reflect => reflect.id === id);
+     }
+     /**
+      * @returns {object} returns all reflections object
+      */
+    findAll(){
+        return this.reflections;
+    }
+    /**
+     * @param {uuid} id
+     * @param {object} data
+     */
+    update(id, data){
+        const refelction = this.findOne(id);
+        const index = this.reflections.indexOf(refelction);
+
+        this.reflections[index].succes = data['succes'] || refelction.succes;
+        this.reflections[index].lowPoint = data['lowPoint'] || reflection.lowPoint;
+        this.reflections[index].takeAway = data['takeAway'] || refelction.takeAway;
+        this.reflections[index].modifiedDate = moment.now()
+
+        return this.reflections[index];
+    }
+
+    /**
+     * 
+     * @params {uuid} id
+     */
+    delete (id){
+
+        const reflection = this.findOne(id);
+        const index = this.reflections.indexOf(reflection);
+
+        this.reflections.splice(index, 1);
+
+        return {};
+    }
 }
+
+export default new Reflection();
